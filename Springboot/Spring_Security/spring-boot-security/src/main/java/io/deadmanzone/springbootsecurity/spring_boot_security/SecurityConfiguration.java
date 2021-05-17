1dee2995c489d46e61e9dd2bfedfec0641ebbe86
Christon Cardoza <christon.antony_cardoza@rsa.com>
2021-05-17 22:51:59 +0530

    basic ldap security

--- Springboot/Spring_Security/spring-boot-security/src/main/java/io/deadmanzone/springbootsecurity/spring_boot_security/SecurityConfiguration.java
+++ Springboot/Spring_Security/spring-boot-security/src/main/java/io/deadmanzone/springbootsecurity/spring_boot_security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,14 +20,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 //	@Autowired
 //	DataSource dataSource;
-	
-	@Autowired
-	UserDetailsService userDetailsService;
-	
+
+//	@Autowired
+//	UserDetailsService userDetailsService;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 //		auth.inMemoryAuthentication()
 //		.withUser("root")
 //		.password("root")
@@ -35,7 +36,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 //		.withUser("toor")
 //		.password("toor")
 //		.roles("ADMIN");
-		
+
 //		auth.jdbcAuthentication()
 //		.dataSource(dataSource)
 //		.withDefaultSchema()
@@ -49,24 +50,40 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 //				.password("pass")
 //				.roles("ADMIN")
 //		);
-		
+
 //		auth.jdbcAuthentication()
 //		.dataSource(dataSource);
-		
-		auth.userDetailsService(userDetailsService);
-		
+
+//		auth.userDetailsService(userDetailsService);
+
+		auth.ldapAuthentication()
+				.userDnPatterns("uid={0},ou=people")
+				.groupSearchBase("ou=groups")
+				.contextSource()
+				.url("ldap://localhost:8399/dc=springframework,dc=org")
+				.and()
+				.passwordCompare()
+				.passwordEncoder(new LdapShaPasswordEncoder())
+				.passwordAttribute("userPassword");
+
 
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
-		
-		http.authorizeRequests()
-		.antMatchers("/admin").hasAnyRole("ADMIN")
-		.antMatchers("/user").hasAnyRole("ADMIN","USER")
-		.antMatchers("/").permitAll()
-		.and().formLogin();
+
+//		http.authorizeRequests()
+//		.antMatchers("/admin").hasAnyRole("ADMIN")
+//		.antMatchers("/user").hasAnyRole("ADMIN","USER")
+//		.antMatchers("/").permitAll()
+//		.and().formLogin();
+
+		http
+				.authorizeRequests()
+				.anyRequest().fullyAuthenticated()
+				.and()
+				.formLogin();
 	}
 
 	@Bean
