ea6290697f71527ff3026b71ed6dd06ed47667ae
Christon Cardoza <christon.antony_cardoza@rsa.com>
2022-12-29 17:11:15 +0530

    feat: Builder Pattern

--- DesignPatterns/src/Creational/Builder/Client.java
+++ DesignPatterns/src/Creational/Builder/Client.java
@@ -5,25 +5,34 @@ import java.time.LocalDate;
 //This is our client which also works as "director"
 public class Client {
 
-	public static void main(String[] args) {
-		System.out.print("Hello");
-	}
+    public static void main(String[] args) {
+        User user = createUser();
+        UserDTOBuilder builder = new UserWebDTOBuilder();
 
-	/**
-	 * Returns a sample user.
-	 */
-	public static User createUser() {
-		User user = new User();
-		user.setBirthday(LocalDate.of(1960, 5, 6));
-		user.setFirstName("Ron");
-		user.setLastName("Swanson");
-		Address address = new Address();
-		address.setHouseNumber("100");
-		address.setStreet("State Street");
-		address.setCity("Pawnee");
-		address.setState("Indiana");
-		address.setZipcode("47998");
-		user.setAddress(address);
-		return user;
-	}
+        UserDTO dto = directBuild(builder, user);
+        System.out.println(dto);
+    }
+
+    // Director
+    private static UserDTO directBuild(UserDTOBuilder builder, User user) {
+        return builder.withFirstName(user.getFirstName()).withLastName(user.getLastName()).withAddress(user.getAddress()).withBirthday(user.getBirthday()).build();
+    }
+
+    /**
+     * Returns a sample user.
+     */
+    public static User createUser() {
+        User user = new User();
+        user.setBirthday(LocalDate.of(1996, 5, 14));
+        user.setFirstName("Christon");
+        user.setLastName("Cardoza");
+        Address address = new Address();
+        address.setHouseNumber("100");
+        address.setStreet("State Street");
+        address.setCity("Pawnee");
+        address.setState("Indiana");
+        address.setZipcode("47998");
+        user.setAddress(address);
+        return user;
+    }
 }
