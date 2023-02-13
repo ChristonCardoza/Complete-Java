12a8e2059e5e77d4252c7f9e2f69713789f529b9
Christon Cardoza <christon.cardoza@netwitness.com>
2023-02-13 22:57:29 +0530

    Feat: Spring Security

---
+++ SpringFramework/src/main/java/com/spring06security/oauth/OauthSecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.spring06security.oauth;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class OauthSecurityConfiguration {
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests().anyRequest().authenticated();
+
+        http.oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
