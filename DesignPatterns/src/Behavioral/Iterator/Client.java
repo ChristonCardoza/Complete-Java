4502e59f32424e57e05f6e8d055f817094dace1f
Christon Cardoza <christon.cardoza@netwitness.com>
2023-04-22 23:04:03 +0530

    fix: Iterator Design Pattern

---
+++ DesignPatterns/src/Behavioral/Iterator/Client.java
@@ -0,0 +1,13 @@
+package Behavioral.Iterator;
+
+public class Client {
+
+	public static void main(String[] args) {
+
+		Iterator<ThemeColor> iter = ThemeColor.getIterator();
+		while (iter.hasNext()){
+			System.out.println(iter.next());
+		}
+	}
+
+}
