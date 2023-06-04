0fa214fd1065ca0d15f564e37e4ed31f98e4ef8f
Christon Cardoza <christoncardoza96@gmail.com>
2023-06-04 23:19:04 +0530

    RestTemplate need to re-visit

---
+++ SpringFramework/spring09RestTemplate/src/main/java/com/resttemplate/Spring09RestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.resttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09RestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring09RestTemplateApplication.class, args);
+    }
+
+}
